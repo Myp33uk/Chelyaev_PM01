c00a0c778bc067adbb6cffee158ded9d8d282026
Myp33uk <nikita.chelyaev123@mail.ru>
2022-01-25 04:34:14 +0300

    Добавьте файлы проекта.

---
+++ Chelyaev_PM01/MainWindow.xaml.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Input;
+using LibTasks;
+
+namespace Chelyaev_PM01
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void Task1_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                int rezult = Moduls.Task1(int.Parse(inputTask1.Text));
+                if (rezult > 0) outputTask1.Text = "Первая цифра больше второй";
+                if (rezult < 0) outputTask1.Text = "Первая цифра меньше второй";
+                if (rezult == 0) outputTask1.Text = "Первая цифра равна второй";
+            }
+            catch
+            {
+                MessageBox.Show("Неверные данные", "Ошибка", MessageBoxButton.OK);
+            }
+        }
+
+        private void Task2_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                int a = int.Parse(inputATask2.Text);
+                int b = int.Parse(inputBTask2.Text);
+                int c = int.Parse(inputCTask2.Text);
+                outputTask2.Text = Moduls.Task2(a, b, c).ToString();
+            }
+            catch
+            {
+                MessageBox.Show("Неверные данные", "Ошибка", MessageBoxButton.OK);
+            }
+        }
+        private void Task3_Click(object sender, RoutedEventArgs e)
+        {
+            if (!int.TryParse(inputColumnsTask3.Text, out int lenght))
+            {
+                MessageBox.Show("Неверные данные", "Ошибка", MessageBoxButton.OK);
+                return;
+            }
+            outputTask3.Text = string.Empty;
+            outputMatrTask3.Text = string.Empty;
+            int[] array = new int[lenght];
+            Random rnd = new Random();
+            for (int i = 0; i < lenght; i++)
+            {
+                array[i] = rnd.Next(-10, 101);
+                outputMatrTask3.Text += array[i].ToString() + ", ";
+            }
+            Moduls.Task3(array, out int count, out int sum);
+            outputTask3.Text = "Сумма: " + sum + "   Количество: " + count;
+        }
+
+        private int[,] _matrix;
+        private void Task4CreateMatr_Click(object sender, RoutedEventArgs e)
+        {
+            //Проверяем введенные данные
+            if (!int.TryParse(rowKol.Text, out int row) || !int.TryParse(colCount.Text, out int column))
+            {
+                MessageBox.Show("Неверные данные", "Ошибка", MessageBoxButton.OK);
+                return;
+            }
+            //Заполняем массив случайными числами
+            _matrix = new int[row, column];
+            Random rnd = new Random();
+            for (int i = 0; i < row; i++)
+            {
+                for (int j = 0; j < column; j++)
+                {
+                    _matrix[i, j] = rnd.Next(-10, 11);
+                }
+            }
+            //Выводим массив на форму
+            matrTable.ItemsSource = VisualArray.ToDataTable(_matrix).DefaultView;
+            outputTask4.Text = string.Empty;
+        }
+        private void Task4Rezult_Click(object sender, RoutedEventArgs e)
+        {
+            if (_matrix == null || _matrix.Length == 0)
+            {
+                MessageBox.Show("Создайте матрицу!", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+            outputTask4.Text = "Номер строки: " + Moduls.Task4(_matrix).ToString();
+        }
+
+        private void TextBox_PrewInput(object sender, TextCompositionEventArgs e)
+        {
+            if (!int.TryParse(e.Text, out _)) e.Handled = true;
+        }
+
+        private void Exit_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void Info_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show(
+                "Разработчик - Челяев Никита ИСП-31\nЗадания: \n\n" +
+                "1. Ввести двузначное число. Определить: какая из его цифр больше: первая или вторая\n\n" +
+                "2. Ввести три целых числа. Подсчитать кол-во чисел, которые являются четными\n\n" +
+                "3. Найти количество и сумму тех членов массива а1,..,аn, которые делятся на 5 и не делятся на 7.\n\n" +
+                "4. Дана целочисленная матрица размера M * N. Найти номер последней из ее строк, содержащих только четные числа.Если таких строк нет, то вывести 0.",
+                "Задания", MessageBoxButton.OK, MessageBoxImage.Information);
+        }
+    }
+}
---
+++ LibTasks/Moduls.cs
@@ -0,0 +1,72 @@
+﻿using System;
+
+namespace LibTasks
+{
+    public class Moduls
+    {
+        /// <summary>
+        /// Определяет какая цифра двухзначного числа больше - первая или вторая
+        /// </summary>
+        /// <param name="number"> двухзначное число</param>
+        /// <returns>возвращает разницу между цифрами. Положительный результат говорит о том,
+        /// что первая цифра больше, отрицательный, что вторая цифра больше, а 0 означает, что цифры равны</returns>
+        public static int Task1(int number)
+        {
+            return number / 10 - number % 10;
+        }
+        /// <summary>
+        /// Вводят 3 числа
+        /// Считает количество чётных цифр
+        /// </summary>
+        /// <param name="firstArg"> первое число</param>
+        /// <param name="secondArg">второе число</param>
+        /// <param name="thirdArg"> третье число</param>
+        /// <returns>Возвращает количество чётных чисел</returns>
+        public static int Task2(int firstArg, int secondArg, int thirdArg)
+        {
+            int count = 0;
+            if (firstArg % 2 == 0) count++;
+            if (secondArg % 2 == 0) count++;
+            if (thirdArg % 2 == 0) count++;
+            return count;
+        }
+        /// <summary>
+        ///  Находит количество и сумму тех членов массива, которые делятся на 5 и не делятся на 7.
+        /// </summary>
+        /// <param name="array">массив</param>
+        /// <param name="count">количество удовлетворяющих условию чисел</param>
+        /// <param name="sum">сумма удовлетворяющих условию чисел</param>
+        public static void Task3(int[] array, out int count, out int sum)
+        {
+            count = 0;
+            sum = 0;
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] % 5 == 0 && array[i] / 7 != 0)
+                {
+                    count++;
+                    sum += array[i];
+                }
+            }
+        }
+        /// <summary>
+        /// Находит номер последней из строк матрицы, содержащих только четные числа.
+        /// </summary>
+        /// <param name="matrix">матрица</param>
+        /// <returns>Возвращает 0 в случае, если строк, удовлетворяющих условию, нет</returns>
+        public static int Task4(int[,] matrix)
+        {
+            int row = 0;
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                int count = 0;
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    if (matrix[i, j] % 2 == 0) count++;
+                }
+                if (count == matrix.GetLength(1)) row = i + 1;
+            }
+            return row;
+        }
+    }
+}
